b9398c13ed7faca654927b02c8bf8d0c389d5816
susanne <susanne_w@gmx.de>
2025-11-27 10:32:07 +0100

    menu die 2.

--- factory/main/app.h
+++ factory/main/app.h
@@ -13,6 +13,22 @@
 // limitations under the License.
 // menu change
 
+ url=https://github.com/melmager/smart-sdstick/blob/cb7d74c096a137aad936d6102b00fb0b41f3389d/factory/main/app.h
+// Copyright 2020-2021 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// menu change
+
 
 #pragma once
 
@@ -136,7 +152,7 @@ static  const _app_driver[] =
         .p_queue_hdl = &g_qr_queue_hdl,
         .flags.restart_after_deinit = true,
     },
-    
+
 
 };
 
